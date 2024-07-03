e6c813346a5786a2f2844d3d5a9fb128917acf82
Jovita08 <joviremen08@gmail.com>
2024-07-03 14:24:34 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+//Siddarthan - 212221040154
+export default defineConfig({
+  plugins: [react()],
+  base: 'Toggle-Visibility-Feature-D2-A1'
+})
